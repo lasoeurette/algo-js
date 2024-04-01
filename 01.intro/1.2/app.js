46a60366aafaa5eaf849abb11abe05c732179d2f
Lasoeurette <lambertjustine.work@gmail.com>
2024-04-01 17:42:03 +0200

    oubli d'un espace

--- 01.intro/1.2/app.js
+++ 01.intro/1.2/app.js
@@ -2,6 +2,6 @@
     let name = 'Justine';
         let firstName = 'Lambert';
             let city = 'Dave';
-            console.log("My name is" + " " + name + " " + firstName + " and I live in" + city);
+            console.log("My name is" + " " + name + " " + firstName + " and I live in " + city);
 })();
 
