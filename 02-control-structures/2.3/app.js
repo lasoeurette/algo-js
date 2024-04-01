739ae6d2c77666598261fbab875fbf3c9805bd2c
Lasoeurette <lambertjustine.work@gmail.com>
2024-04-01 19:51:52 +0200

    essai exercice 3

--- 02-control-structures/2.3/app.js
+++ 02-control-structures/2.3/app.js
@@ -1,3 +1,14 @@
 (() => {
-    // YOUR CODE HERE
+let a = 2;
+
+while (a <= 100) 
+{ 
+    console.log(a);
+    a += 2;
+}
+})();
+
+(() => {
+for (let b = 2; b <= 100; b +=2) {
+    console.log(b);}
 })();
