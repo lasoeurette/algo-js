4a7765f0115aed1ba232d16735bdcdf2ce859152
Lasoeurette <lambertjustine.work@gmail.com>
2024-04-01 19:20:38 +0200

    exercices 2.1 et 2.2

--- 02-control-structures/2.1/app.js
+++ 02-control-structures/2.1/app.js
@@ -1,7 +1,8 @@
 (() => {
     let age = prompt('Enter your age');
-    if (age >= 18)
+    if (age >= 18) {
         console.log("You're an adult");
-    else (age <= 18);
-        console.log("You are not an adult");
-})();
+    }
+    else {
+        console.log("You are not an adult");}
+    })();
--- 02-control-structures/2.2/app.js
+++ 02-control-structures/2.2/app.js
@@ -2,8 +2,12 @@
     let min = prompt ('Enter a minimum number');
     let max = prompt ('Enter a maximal number');
     let current = prompt ('Enter a current number');
-        if (current > min || current < max);
-        {    if (min > max);
-            console.log( "You're dumb, leave the site" );
-        }
-})();
+
+    if ( current > min && current < max) { 
+         console.log("GG !");  
+            }
+            else if ( min > max || max < min) 
+            {
+                console.log("You're dumb, leave the site");}
+        
+    })();
