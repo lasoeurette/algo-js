07c82bd13e357f73887a680ff5a52695a15ab259
Lasoeurette <lambertjustine.work@gmail.com>
2024-04-02 19:34:53 +0200

    essai encore

--- essai/app.js
+++ essai/app.js
@@ -1,11 +1,9 @@
 (() => {
-  
-    let tableau = [12, 22, 333, 544, 15];
+  let tableau = [112, 222, 333, 544, 615];
     let somme = 0;
       for ( let i = 0; i < tableau.length; i++){
           somme += tableau[i];
        }
        const moyenne = somme / tableau.length;
-       console.log("La moyenne du tableau est :", moyenne);
-      
+       console.log("La moyenne du tableau est :", moyenne);   
     })();
