3ca065710b4261afb04bca1fe8636656530f4d55
Lasoeurette <lambertjustine.work@gmail.com>
2024-04-02 10:48:32 +0200

    création des fichiers pour exercices .03

--- 02-control-structures/2.6/app.js
+++ 02-control-structures/2.6/app.js
@@ -1,3 +1,21 @@
 (() => {
-    // YOUR CODE HERE
+    let day = prompt("Choisis un chiffre de 1 à 7");
+
+      if (day === "1")  {
+           console.log("Lundi"); 
+        } else if (day === "2") {
+        console.log("Mardi"); }
+         else if (day === "3") {
+        console.log("Mercredi");
+    } else if (day === "4") {
+        console.log("Jeudi");
+    } else if (day === "5") {
+        console.log("Vendredi");
+    } else if (day === "6") {
+        console.log("Samedi");
+    } else if (day === "7") {
+        console.log("Dimanche");
+    } else {
+        console.log("Veuillez choisir un chiffre entre 1 et 7.")
+    }
 })();
--- 02-control-structures/2.7/app.js
+++ 02-control-structures/2.7/app.js
@@ -1,3 +1,20 @@
 (() => {
-    // YOUR CODE HERE
+    let sum = 0; 
+
+    let n = parseInt(prompt("Entrez un nombre :"));
+    
+    if (!isNaN(n) && n > 0) { 
+        for (let i = 0; i < n; i++) { 
+            let number = parseInt(prompt("Entrez un nouveau nombre :")); 
+            if (!isNaN(number)) { 
+                sum += number; // 
+            } else {
+                alert("Veuillez entrer un nombre valide."); 
+                i--; //
+            }
+        }
+        alert("La somme des nombres est : " + sum); 
+        alert("Veuillez entrer un nombre valide et positif."); 
+    }
+    
 })();
