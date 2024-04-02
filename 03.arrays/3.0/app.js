b318de04dbe6e05bd31c606dbab6063f36060c8d
Lasoeurette <lambertjustine.work@gmail.com>
2024-04-02 12:35:28 +0200

    exo 3.0 et 3.1

--- 03.arrays/3.0/app.js
+++ 03.arrays/3.0/app.js
@@ -1,3 +1,8 @@
 (() => {
+let classMember = ['Illies', 'Maryam', 'Nathanaël', 'Zahra', 'Dorian', 'Adrien', 'Jean-Emmanuel', 'Adrien.C', 'Caroline', 'Tom', 'Stacy', 'Dylan', 'Justine.F', 'Mohamed', 'Lyne', 'Dzheylyan', 'Ludovic', 'Justine', 'Denis', 'Jordan', 'Nataliia', 'Angel', 'Isabelle', 'Youris', 'Giovanni', 'Arnaud.V', 'Julie'];
 
+for (let membres of classMember) 
+{ 
+    console.log(membres);
+}
 })();
--- 03.arrays/3.1/app.js
+++ 03.arrays/3.1/app.js
@@ -1,4 +1,15 @@
 (() => {
-
-  
+let tableau = [];
+let number = [1, 2, 3, 4, 5];
+for (let number of tableau) { 
+    console.log(number);
+}
     })();
+
+    (() => {
+        let tableau = [];
+        let number = [101, 102, 103];
+        for (let number of tableau) { 
+            console.log(number);
+        }
+            })();
