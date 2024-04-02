d755cd874dec1fbeeec5896fb0b87b4033cad17d
Lasoeurette <lambertjustine.work@gmail.com>
2024-04-02 12:39:28 +0200

    verif exo 3.1

--- 03.arrays/3.1/app.js
+++ 03.arrays/3.1/app.js
@@ -1,9 +1,10 @@
 (() => {
-let tableau = [];
-let number = [1, 2, 3, 4, 5];
-for (let number of tableau) { 
-    console.log(number);
+let tableau = [1, 2, 3, 4, 5];
+let somme = 0;
+for (let nombre of tableau) { 
+    somme += nombre;
 }
+    console.log("La somme de tous les éléments du tableau est : " + somme);
     })();
 
     (() => {
